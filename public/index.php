3e87d0bf59e79e95ccf1718fbe60ce2a9b7c4b13
kriptonuz <kripton253@gmail.com>
2021-02-01 15:49:28 +0200

    require classes

--- public/index.php
+++ public/index.php
@@ -1,7 +1,8 @@
 <?php
+require __DIR__ . '../Api.php';
+require __DIR__ . '../NewChannelPostHandler.php';
 
 $token = getenv('BOT_TOKEN');
-
 $api = new Api($token);
 
 $api->sendMessage(47543915, 'test');
